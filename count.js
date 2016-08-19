34705fca17e4c01ea29bf2ed7781252c723d621b
hook <gabrielhook@gmail.com>
2016-08-18 23:49:43 -0700

    add maximums

--- count.js
+++ count.js
@@ -1,3 +1,5 @@
+const maximums = require('./maximums');
+
 const words = process.argv.slice(2);
 const counts = words
     .join('')
@@ -7,7 +9,11 @@ const counts = words
         return map;
     }, {});
 
+const maximum = (key) => maximums[key] || 3;
+const colorify = (key) => (string) =>
+    `\x1b[${(maximum(key) === counts[key])? 33: (maximum(key) < counts[key])? 31: 32}m${string}\x1b[0m`;
+
 Object
     .keys(counts)
     .sort()
-    .forEach((key) => console.log(`${key}: ${counts[key]}`));
+    .forEach((key) => console.log(`${key}: ${colorify(key)(counts[key])}`));
